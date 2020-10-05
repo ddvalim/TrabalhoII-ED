e9b51fca5b4abde8968fb26df655838928987f58
Diovana Rodrigues Valim <53877301+ddvalim@users.noreply.github.com>
2020-10-05 17:13:53 -0300

    Add files via upload

---
+++ Elemento.py
@@ -0,0 +1,21 @@
+class Elemento:
+    def __init__(self, numero):
+        self.__numero = numero
+        self.__anterior = None
+        self.__proximo = None
+
+
+    def set_proximo(self, element:object):
+        self.__proximo = element
+
+    
+    def set_anterior(self, element:object):
+        self.__anterior = element
+    
+
+    def get_proximo(self):
+        return self.__proximo
+    
+
+    def get_anterior(self):
+        return self.__anterior
