7170b460753382471d35dc8de810a65e28be2ee9
Diovana Rodrigues Valim <diovanavalimm@gmail.com>
2020-10-18 11:52:34 -0300

    update test and fix bugs

--- test.py
+++ test.py
@@ -83,20 +83,3 @@ print('Busca')
 print('*'*15)
 
 print(l.buscar(12))
-
-'''
-l.inserir_antes_atual(2)
-assert l.acessar_atual().get_valor() == 2
-el = l.acessar_atual()
-l.ir_para_final()
-assert el != l.acessar_atual()
-assert l.buscar(2)
-assert l.buscar(5)
-assert not l.buscar(77)
-l.excluir_primeiro()
-l.ir_para_inicio()
-assert l.acessar_atual().get_valor() == 4
-l.excluir_ultimo()
-l.ir_para_final()
-assert l.acessar_atual().get_valor() == 2
-'''
