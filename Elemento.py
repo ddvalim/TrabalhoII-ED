47142b0c6f89baeb293d1c220197c86a9f5665b2
Diovana Rodrigues Valim <53877301+ddvalim@users.noreply.github.com>
2020-10-18 10:50:55 -0300

    pep-8 adjust

--- Elemento.py
+++ Elemento.py
@@ -13,7 +13,7 @@ class Elemento:
         return self.__valor
 
     
-    def set_anterior(self, element:object):
+    def set_anterior(self, element:Elemento):
         self.__anterior = element
     
 
